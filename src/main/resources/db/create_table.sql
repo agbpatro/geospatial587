c60091b90923578fe49b81b5c6ddc36c1edc6e66
Abhishek Patro <abhishek@spotify.net>
2016-10-03 02:12:00 -0700

    add insert scripts

--- src/main/resources/db/create_table.sql
+++ src/main/resources/db/create_table.sql
@@ -30,3 +30,20 @@ CREATE TABLE PERSONAD (
   adId     INTEGER,
   personId INTEGER
 );
+
+
+INSERT INTO ad VALUES (1, 'Made in India', '', 'video', 1, 0, 9, 10000, 9991, 'USA', '((0,0),(0,10),(10,10),(0,0))');
+INSERT INTO ad VALUES (3, 'Harry Potter', '', 'video', 3, 0, 6, 10000, 9994, 'USA', '((0,0),(0,10),(10,10),(0,0))');
+INSERT INTO ad VALUES (2, 'Swades', '', 'video', 2, 0, 7, 10000, 9993, 'USA', '((0,0),(0,10),(10,10),(0,0))');
+INSERT INTO ad VALUES (4, 'Secret Life of Pets', '', 'video', 4, 0, 6, 10000, 9994, 'USA', '((0,0),(0,10),(10,10),(0,0))');
+
+INSERT INTO person VALUES (1, 21, 'Karanjeet Singh Sarna', 'USA');
+INSERT INTO person VALUES (2, 25, 'Abhishek Patro', 'USA');
+INSERT INTO person VALUES (3, 24, 'Shiven Saiwal', 'USA');
+
+
+INSERT INTO personad VALUES (1, 1, 1);
+INSERT INTO personad VALUES (2, 2, 1);
+INSERT INTO personad VALUES (3, 3, 2);
+INSERT INTO personad VALUES (4, 4, 2);
+
