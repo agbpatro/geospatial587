d001702450452e37a94c9a96571e70a31e7e5bde
Abhishek Patro <abhishek@spotify.net>
2016-10-18 10:47:39 -0700

    select ad with impression

--- src/main/java/service/db/impl/AdDaoImpl.java
+++ src/main/java/service/db/impl/AdDaoImpl.java
@@ -181,7 +181,8 @@ public class AdDaoImpl implements AdDao {
       while (rs.next()) {
         adList.add(getAd(rs));
       }
-      return adList.get(rn.nextInt(adList.size()));
+      Ad selectedAd = adList.get(rn.nextInt(adList.size()));
+      return clickAd(selectedAd, IMPRESSION);
     } catch (SQLException e) {
       LOG.error("Error getting ad by name", e);
     } finally {
