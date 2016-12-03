623a0c6c0a81e13c70603182519f0caf3d8972d4
Abhishek Patro <abhishek@spotify.net>
2016-12-02 17:33:27 -0800

    demo ads

--- src/main/java/service/db/impl/AdDaoImpl.java
+++ src/main/java/service/db/impl/AdDaoImpl.java
@@ -251,6 +251,7 @@ public class AdDaoImpl implements AdDao {
     Connection conn = getConnection();
     List<Ad> adList = new ArrayList<>();
     Ad selectedAd = null;
+    /*
     if (model.getType().equals(RANDOM)) {
         selectedAd = getRandomAd(getAllAds());
     } else if (model.getType().equals(NN)) {
@@ -288,6 +289,8 @@ public class AdDaoImpl implements AdDao {
         selectedAd = nearestNeighbor(model);
       }
     }
+    */
+    selectedAd = getRandomAd(getAllAds());
     return clickAd(selectedAd, IMPRESSION);
     //return selectedAd;
 
